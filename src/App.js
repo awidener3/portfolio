e58e58e0b8c97a9af5c53e572ecf7d40c2200741
Alex Widener <awidener3@gmail.com>
2022-03-02 17:44:12 -0500

    added react-bootstrap and added example nav

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// Import components here
+import Navigation from './components/Navigation';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+	return (
+		<div className="App">
+			<Navigation />
+		</div>
+	);
+}
+
+export default App;
