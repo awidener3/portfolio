bfe92cecab9c6990c55c7504ce4d9e09a0220b54
Alex Widener <awidener3@gmail.com>
2022-03-04 19:44:34 -0500

    added animation to project cards

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -12,25 +12,32 @@ export default function ProjectCard({
 }) {
 	return (
 		// <div className="m-4 custom-card">
-		<div className="m-4 custom-card">
-			<div className={isNew === 'true' ? 'ribbon ribbon-top-right' : ''}>
-				<span>{isNew === 'true' ? 'New' : ''}</span>
+		<a href="" className="project-card-link">
+			<div
+				className="m-4 custom-card d-flex justify-content-center align-items-center"
+				style={{ backgroundImage: `url(${img})` }}
+			>
+				<div
+					className={
+						isNew === 'true' ? 'ribbon ribbon-top-right' : ''
+					}
+				>
+					<span>{isNew === 'true' ? 'New' : ''}</span>
+				</div>
+
+				<div className="p-3 custom-card-text d-flex justify-content-center align-items-center text-center">
+					<h3 className="custom-card-title">
+						<a
+							href={liveUrl}
+							target="_blank"
+							rel="noreferrer"
+							className="custom-card-link"
+						>
+							{title}
+						</a>
+					</h3>
+				</div>
 			</div>
-			<a href={liveUrl} target="_blank" rel="noreferrer">
-				<img className="card-image" src={img} alt={alt} />
-			</a>
-			<div className="p-3 custom-card-text d-flex justify-content-center align-items-center text-center">
-				<h3 className="custom-card-title">
-					<a
-						href={gitHubUrl}
-						target="_blank"
-						rel="noreferrer"
-						className="custom-card-link"
-					>
-						{title}
-					</a>
-				</h3>
-			</div>
-		</div>
+		</a>
 	);
 }
