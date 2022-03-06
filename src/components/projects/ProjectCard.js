09a428e47d9322ee9bc693ee190066f1646e8578
Alex Widener <awidener3@gmail.com>
2022-03-06 08:56:33 -0500

    separated projectCard data from file and placed in it's own projectData.js file

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,34 +1,29 @@
 import React from 'react';
 import './projectCard.css';
 
-export default function ProjectCard({
-	title,
-	languages,
-	liveUrl,
-	gitHubUrl,
-	img,
-	alt,
-	isNew,
-}) {
+export default function ProjectCard({ project }) {
 	return (
-		// <div className="m-4 custom-card">
 		<div
 			className="m-4 custom-card d-flex justify-content-center align-items-center"
-			style={{ backgroundImage: `url(${img})` }}
+			style={{ backgroundImage: `url(${project.img})` }}
 		>
-			<div className={isNew === 'true' ? 'ribbon ribbon-top-right' : ''}>
-				<span>{isNew === 'true' ? 'New' : ''}</span>
+			<div
+				className={
+					project.isNew === true ? 'ribbon ribbon-top-right' : ''
+				}
+			>
+				<span>{project.isNew === true ? 'New' : ''}</span>
 			</div>
 
 			<div className="p-3 custom-card-text d-flex justify-content-center align-items-center text-center">
 				<h3 className="custom-card-title">
 					<a
-						href={liveUrl}
+						href={project.liveUrl}
 						target="_blank"
 						rel="noreferrer"
 						className="custom-card-link"
 					>
-						{title}
+						{project.title}
 					</a>
 				</h3>
 			</div>
--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import chronicler from '../../assets/images/chronicler-small.png';
-import weatherDashboard from '../../assets/images/weather-dashboard-small.png';
-import techBlog from '../../assets/images/tech-blog-small.png';
-import restaurantPage from '../../assets/images/restaurant-page-preview.png';
-import fourStor from '../../assets/images/fourstor-preview.png';
-import odinLibrary from '../../assets/images/library-preview.png';
-import teamProfile from '../../assets/images/team-profile-generator-preview.png';
 import ProjectCard from './ProjectCard';
+import { projectData } from './projectData';
 
 export default function Projects() {
 	return (
@@ -14,76 +8,9 @@ export default function Projects() {
 			<h2 className="pe-3 border-end text-end me-5">Projects</h2>
 
 			<div className="section-content container-fluid d-flex flex-wrap justify-content-center">
-				{/* Chronicler */}
-				<ProjectCard
-					title="The Chronicler"
-					languages="MySQL, Handlebars, NodeJS"
-					liveUrl="https://thechronicler.herokuapp.com/"
-					gitHubUrl="https://github.com/xndrbrgs/the_chronicler"
-					img={chronicler}
-					alt="a preview of The Chronicler"
-					isNew="true"
-				/>
-				{/* Weather Dash */}
-				<ProjectCard
-					title="Weather Dashboard"
-					languages="JavaScript"
-					liveUrl="https://awidener3.github.io/weather-dashboard/"
-					gitHubUrl="https://github.com/awidener3/weather-dashboard"
-					img={weatherDashboard}
-					alt="a preview of a weather dashboard project"
-					isNew="false"
-				/>
-				{/* Tech Blog */}
-				<ProjectCard
-					title="Tech Blog"
-					languages="JavaScript, SQL, MVC"
-					liveUrl="https://awidener-tech-blog.herokuapp.com/"
-					gitHubUrl="https://github.com/awidener3/tech-blog"
-					img={techBlog}
-					alt="preview of a tech blog application"
-					isNew="false"
-				/>
-				{/* Restaurant Page */}
-				<ProjectCard
-					title="Restaurant Page"
-					languages="JavaScript, NodeJS, Webpack"
-					liveUrl="https://awidener3.github.io/restaurant-page/"
-					gitHubUrl="https://github.com/awidener3/restaurant-page"
-					img={restaurantPage}
-					alt="a preview of a restaurant website project"
-					isNew="false"
-				/>
-				{/* fourSTOR */}
-				<ProjectCard
-					title="fourSTOR"
-					languages="JavaScript, Foundations"
-					liveUrl="https://pr1me-e1gh7.github.io/fourstor/"
-					gitHubUrl="https://github.com/pr1me-e1gh7/fourstor"
-					img={fourStor}
-					alt="preview of fourStor"
-					isNew="false"
-				/>
-				{/* Odin's Library */}
-				<ProjectCard
-					title="Odin's Library"
-					languages="HTML, CSS, JavaScript"
-					liveUrl="https://awidener3.github.io/odin-library/"
-					gitHubUrl="https://github.com/awidener3/odin-library"
-					img={odinLibrary}
-					alt="a preview of a library project"
-					isNew="false"
-				/>
-				{/* Team Profile Generator */}
-				<ProjectCard
-					title="Team Profile Generator"
-					languages="JavaScript, Inquirer, Jest"
-					liveUrl="https://github.com/awidener3/team-profile-generator"
-					gitHubUrl="https://github.com/awidener3/team-profile-generator"
-					img={teamProfile}
-					alt="preview of a team profile generator"
-					isNew="false"
-				/>
+				{projectData.map((project, index) => (
+					<ProjectCard project={project} key={index} />
+				))}
 			</div>
 		</div>
 	);
