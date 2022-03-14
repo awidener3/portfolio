6b1611ad3f8bddc584e7c1a1eb880c98e48b0e9e
Alex Widener <awidener3@gmail.com>
2022-03-14 09:39:58 -0400

    :construction: Added live link and repo link to project card

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -16,16 +16,25 @@ export default function ProjectCard({ project }) {
 			</div>
 
 			<div className="p-3 custom-card-text d-flex justify-content-center align-items-center text-center">
-				<h3 className="custom-card-title">
+				<h3 className="custom-card-title">{project.title}</h3>
+				<div className="hide">
 					<a
 						href={project.liveUrl}
 						target="_blank"
 						rel="noreferrer"
-						className="custom-card-link"
+						className="btn btn-primary p-1 m-1 custom-btn w-100 mx-auto"
 					>
-						{project.title}
+						Live
 					</a>
-				</h3>
+					<a
+						href={project.gitHubUrl}
+						target="_blank"
+						rel="noreferrer"
+						className="btn btn-primary p-1 m-1 custom-btn w-100 mx-auto"
+					>
+						Repo
+					</a>
+				</div>
 			</div>
 		</div>
 	);
