5d2e267e7aac93c27430558986b6eb1fe7542473
Alex Widener <awidener3@gmail.com>
2022-03-17 22:21:57 -0400

    Fixed project folder case error and changed project justification to start

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -7,7 +7,7 @@ export default function Projects() {
 		<div className="container-fluid">
 			<h2 className="pe-3 border-end text-end me-5">Projects</h2>
 
-			<div className="section-content container-fluid d-flex flex-wrap justify-content-center">
+			<div className="section-content container-fluid d-flex flex-wrap">
 				{projectData.map((project, index) => (
 					<ProjectCard project={project} key={index} />
 				))}
